ac35e1c754cf6b7067ce993be5b83d874946623c
Miladin Joksic <miladin.joksic@primeholding.com>
2018-06-13 14:03:53 +0200

    Added initial logging API REST client

--- client/Lykke.AlgoStore.Service.Logging.Client/ILoggingClient.cs
+++ client/Lykke.AlgoStore.Service.Logging.Client/ILoggingClient.cs
@@ -1,6 +1,9 @@
 ﻿
 namespace Lykke.AlgoStore.Service.Logging.Client
 {
+    /// <summary>
+    /// Logging API REST client interface
+    /// </summary>
     public interface ILoggingClient
     {
     }
--- client/Lykke.AlgoStore.Service.Logging.Client/LoggingClient.cs
+++ client/Lykke.AlgoStore.Service.Logging.Client/LoggingClient.cs
@@ -1,23 +1,27 @@
 ﻿using System;
+using System.Net.Http;
 using Common.Log;
+using Lykke.Service.Logging.Client.AutorestClient;
 
 namespace Lykke.AlgoStore.Service.Logging.Client
 {
     public class LoggingClient : ILoggingClient, IDisposable
     {
         private readonly ILog _log;
+        private IAlgoStoreLoggingAPI _service;
 
         public LoggingClient(string serviceUrl, ILog log)
         {
             _log = log;
+            _service = new AlgoStoreLoggingAPI(new Uri(serviceUrl), new HttpClient());
         }
 
         public void Dispose()
         {
-            //if (_service == null)
-            //    return;
-            //_service.Dispose();
-            //_service = null;
+            if (_service == null)
+                return;
+            _service.Dispose();
+            _service = null;
         }
     }
 }
