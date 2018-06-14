48c5505ef8b5f5026194071ca8ba0bad5ef3b7f0
Miladin Joksic <miladin.joksic@primeholding.com>
2018-06-14 12:50:12 +0200

    Added new service write methods

--- src/Lykke.AlgoStore.Service.Logging.Services/UserLogService.cs
+++ src/Lykke.AlgoStore.Service.Logging.Services/UserLogService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Lykke.AlgoStore.Service.Logging.Core.Domain;
@@ -18,17 +19,52 @@ namespace Lykke.AlgoStore.Service.Logging.Services
 
         public async Task WriteAsync(IUserLog userLog)
         {
+            ValidateUserLogData(userLog);
+
             await _userLogRepository.WriteAsync(userLog);
         }
 
         public async Task WriteAsync(string instanceId, string message)
         {
+            ValidateInstanceId(instanceId);
+            ValidateMessage(message);
+
             await _userLogRepository.WriteAsync(instanceId, message);
         }
 
         public async Task WriteAsync(string instanceId, Exception ex)
         {
+            ValidateInstanceId(instanceId);
+            ValidateException(ex);
+
             await _userLogRepository.WriteAsync(instanceId, ex);
+        }
+
+        private static void ValidateInstanceId(string instanceId)
+        {
+            if (string.IsNullOrEmpty(instanceId))
+                throw new ValidationException("InstanceId cannot be empty");
+        }
+
+        private static void ValidateMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                throw new ValidationException("Message cannot be empty");
+        }
+
+        private static void ValidateException(Exception exception)
+        {
+            if (exception == null)
+                throw new ArgumentNullException(nameof(exception));
+        }
+
+        private static void ValidateUserLogData([NotNull] IUserLog userLog)
+        {
+            if (userLog == null)
+                throw new ArgumentNullException(nameof(userLog));
+
+            ValidateInstanceId(userLog.InstanceId);
+            ValidateMessage(userLog.Message);
         }
     }
 }
