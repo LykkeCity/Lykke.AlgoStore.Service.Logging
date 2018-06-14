d55b36d2a45df279632cb50c82fa7148a5b25485
Miladin Joksic <miladin.joksic@primeholding.com>
2018-06-14 14:48:30 +0200

    Added additional service tests

--- tests/Lykke.AlgoStore.Service.Logging.Tests/Unit/UserLogServiceTests.cs
+++ tests/Lykke.AlgoStore.Service.Logging.Tests/Unit/UserLogServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using AutoFixture;
 using Lykke.AlgoStore.Service.Logging.Core.Repositories;
 using Lykke.AlgoStore.Service.Logging.Core.Services;
@@ -25,23 +27,73 @@ namespace Lykke.AlgoStore.Service.Logging.Tests.Unit
         }
 
         [Test]
-        public void WriteTest()
+        public void WriteUserLogDataTest()
         {
             _service.WriteAsync(_entitytRequest).Wait();
         }
 
-        private IUserLogService MockValidUserLogService()
+        [Test]
+        public void WriteUserLogDataAsNullWillThrowExceptionTest()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(() => _service.WriteAsync(null));
+        }
+
+        [Test]
+        public void WriteUserLogWithInstanceIdAndMessageTest()
+        {
+            _service.WriteAsync("12345", "Message for 12345").Wait();
+        }
+
+        [Test]
+        public void WriteUserLogWithInvalidInstanceIdWillThrowExceptionTest()
+        {
+            Assert.ThrowsAsync<ValidationException>(() => _service.WriteAsync(string.Empty, "Message for 12345"));
+        }
+
+        [Test]
+        public void WriteUserLogWithNullAsInstanceIdWillThrowExceptionTest()
+        {
+            Assert.ThrowsAsync<ValidationException>(() => _service.WriteAsync(null, "Message for 12345"));
+        }
+
+        [Test]
+        public void WriteUserLogWithInvalidMessageWillThrowExceptionTest()
+        {
+            Assert.ThrowsAsync<ValidationException>(() => _service.WriteAsync("12345", string.Empty));
+        }
+
+        [Test]
+        public void WriteUserLogWithNullAsMessageWillThrowExceptionTest()
+        {
+            Assert.ThrowsAsync<ValidationException>(() => _service.WriteAsync("12345", message: null));
+        }
+
+        [Test]
+        public void WriteUserLogWithInstanceIdAndExceptionTest()
+        {
+            _service.WriteAsync("12345", new Exception("Exception for 12345")).Wait();
+        }
+
+        [Test]
+        public void WriteUserLogWithInvalidExceptionWillThrowExceptionTest()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(() => _service.WriteAsync("12345", ex: null));
+        }
+
+        private static IUserLogService MockValidUserLogService()
         {
             var userLogRepository = MockValidUserLogRepository();
 
-            return  new UserLogService(userLogRepository);
+            return new UserLogService(userLogRepository);
         }
 
-        private IUserLogRepository MockValidUserLogRepository()
+        private static IUserLogRepository MockValidUserLogRepository()
         {
             var repo = new Mock<IUserLogRepository>();
 
             repo.Setup(x => x.WriteAsync(It.IsAny<UserLogRequest>())).Returns(Task.CompletedTask);
+            repo.Setup(x => x.WriteAsync(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
+            repo.Setup(x => x.WriteAsync(It.IsAny<string>(), It.IsAny<Exception>())).Returns(Task.CompletedTask);
 
             return repo.Object;
         }
