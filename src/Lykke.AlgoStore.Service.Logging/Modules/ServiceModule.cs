3a7d7a2b26112a4e50c9e6439093d68526a4630c
Miladin Joksic <miladin.joksic@primeholding.com>
2018-09-10 12:56:32 +0200

    Upgraded packgaes to latest versions

--- src/Lykke.AlgoStore.Service.Logging/Modules/ServiceModule.cs
+++ src/Lykke.AlgoStore.Service.Logging/Modules/ServiceModule.cs
@@ -7,6 +7,10 @@ using Lykke.AlgoStore.Service.Logging.AzureRepositories.Entitites;
 using Lykke.AlgoStore.Service.Logging.Core.Services;
 using Lykke.AlgoStore.Service.Logging.Services;
 using Lykke.AlgoStore.Service.Logging.Settings;
+using Lykke.Common.Log;
+using Lykke.Logs;
+using Lykke.Logs.Loggers.LykkeConsole;
+using Lykke.Sdk.Health;
 using Lykke.SettingsReader;
 using IUserLogRepository = Lykke.AlgoStore.Service.Logging.Core.Repositories.IUserLogRepository;
 using UserLogRepository = Lykke.AlgoStore.Service.Logging.AzureRepositories.UserLogRepository;
@@ -16,30 +20,41 @@ namespace Lykke.AlgoStore.Service.Logging.Modules
     public class ServiceModule : Module
     {
         private readonly IReloadingManager<AppSettings> _appSettings;
-        private readonly ILog _log;
 
-        public ServiceModule(IReloadingManager<AppSettings> appSettings, ILog log)
+        public ServiceModule(IReloadingManager<AppSettings> appSettings)
         {
             _appSettings = appSettings;
-            _log = log;
         }
 
         protected override void Load(ContainerBuilder builder)
         {
             // Do not register entire settings in container, pass necessary settings to services which requires them
-
+            var logFactory = LogFactory.Create().AddConsole();
             var reloadingDbManager = _appSettings.ConnectionString(x => x.AlgoStoreLoggingService.Db.DataStorageConnectionString);
 
-            builder.RegisterInstance(AzureTableStorage<UserLogEntity>.Create(reloadingDbManager, UserLogRepository.TableName, _log));
+            builder.RegisterInstance(AzureTableStorage<UserLogEntity>.Create(reloadingDbManager, UserLogRepository.TableName, logFactory));
 
             builder.RegisterType<UserLogRepository>().As<IUserLogRepository>();
 
-            builder.RegisterInstance<IAlgoClientInstanceRepository>(
-                    AzureRepoFactories.CreateAlgoClientInstanceRepository(reloadingDbManager, _log))
+            builder.Register(x =>
+                {
+                    var log = x.Resolve<ILogFactory>();
+                    var repository = CSharp.AlgoTemplate.Models.AzureRepoFactories
+                        .CreateAlgoClientInstanceRepository(
+                            _appSettings.Nested(r => r.AlgoStoreLoggingService.Db.DataStorageConnectionString),
+                            log.CreateLog(this));
+
+                    return repository;
+                })
+                .As<IAlgoClientInstanceRepository>()
                 .SingleInstance();
 
             builder.RegisterType<UserLogService>()
                 .As<IUserLogService>()
+                .SingleInstance();
+
+            builder.RegisterType<HealthService>()
+                .As<IHealthService>()
                 .SingleInstance();
         }
     }
--- src/Lykke.AlgoStore.Service.Logging/Startup.cs
+++ src/Lykke.AlgoStore.Service.Logging/Startup.cs
@@ -1,25 +1,45 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using AutoMapper;
 using Common.Log;
 using Lykke.AlgoStore.CSharp.AlgoTemplate.Models.Mapper;
 using Lykke.AlgoStore.Security.InstanceAuth;
 using Lykke.AlgoStore.Service.Logging.Filters;
+using Lykke.AlgoStore.Service.Logging.Modules;
 using Lykke.AlgoStore.Service.Logging.Settings;
+using Lykke.Common;
 using Lykke.Common.Api.Contract.Responses;
+using Lykke.Common.ApiLibrary.Swagger;
+using Lykke.Common.Log;
+using Lykke.Logs;
 using Lykke.Sdk;
 using Lykke.SettingsReader;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Converters;
 
 namespace Lykke.AlgoStore.Service.Logging
 {
     public class Startup
     {
         public IConfigurationRoot Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
+
         private ILog _log;
+        private readonly LykkeSwaggerOptions _swaggerOptions = new LykkeSwaggerOptions
+        {
+            ApiTitle = ApiName,
+            ApiVersion = ApiVersion
+        };
+        private const string ApiVersion = "v1";
+        private const string ApiName = "Algo Store Logging API";
+        private string _monitoringServiceUrl;
+        private IHealthNotifier _healthNotifier;
 
         public Startup(IHostingEnvironment env)
         {
@@ -32,44 +52,106 @@ namespace Lykke.AlgoStore.Service.Logging
 
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            var appSettings = Configuration.LoadSettings<AppSettings>();
-
-            services.AddInstanceAuthentication(appSettings.CurrentValue.AlgoStoreLoggingService.LoggingServiceAuth);
-
-            return services.BuildServiceProvider<AppSettings>(options =>
+            try
             {
-                options.ApiTitle = "Algo Store Logging API";
-                options.Logs = ("AlgoStoreLoggingLog", ctx => ctx.AlgoStoreLoggingService.Db.LogsConnectionString);
-            }, swagger => { swagger.OperationFilter<ApiKeyHeaderOperationFilter>(); });
+                services.AddMvc()
+                    .AddJsonOptions(options =>
+                    {
+                        options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                        options.SerializerSettings.ContractResolver =
+                            new Newtonsoft.Json.Serialization.DefaultContractResolver();
+                    });
+
+                services.AddSwaggerGen(options => { options.DefaultLykkeConfiguration(ApiVersion, ApiName); });
+
+                var settingsManager = Configuration.LoadSettings<AppSettings>(x =>
+                {
+                    x.SetConnString(y => y.SlackNotifications.AzureQueue.ConnectionString);
+                    x.SetQueueName(y => y.SlackNotifications.AzureQueue.QueueName);
+                    x.SenderName = $"{AppEnvironment.Name} {AppEnvironment.Version}";
+                });
+
+                var appSettings = settingsManager.CurrentValue;
+
+                if (appSettings.MonitoringServiceClient != null)
+                    _monitoringServiceUrl = appSettings.MonitoringServiceClient.MonitoringServiceUrl;
+
+                services.AddLykkeLogging(
+                    settingsManager.ConnectionString(s => s.AlgoStoreLoggingService.Db.LogsConnectionString),
+                    "AlgoStoreLoggingLog",
+                    appSettings.SlackNotifications.AzureQueue.ConnectionString,
+                    appSettings.SlackNotifications.AzureQueue.QueueName);
+
+                services.AddInstanceAuthentication(appSettings.AlgoStoreLoggingService.LoggingServiceAuth);
+
+                services.ConfigureSwaggerGen(options => options.OperationFilter<ApiKeyHeaderOperationFilter>());
+
+                var builder = new ContainerBuilder();
+
+                builder.RegisterModule(new ServiceModule(settingsManager));
+
+                builder.Populate(services);
+
+                ApplicationContainer = builder.Build();
+
+                var logFactory = ApplicationContainer.Resolve<ILogFactory>();
+                _log = logFactory.CreateLog(this);
+
+                _healthNotifier = ApplicationContainer.Resolve<IHealthNotifier>();
+
+                return new AutofacServiceProvider(ApplicationContainer);
+            }
+            catch (Exception ex)
+            {
+                if (_log == null)
+                    Console.WriteLine(ex);
+                else
+                    _log.Critical(ex);
+                throw;
+            }
         }
 
-        public void Configure(IApplicationBuilder app, ILog log)
+        public void Configure(IApplicationBuilder app, IApplicationLifetime appLifetime)
         {
-            _log = log;
-
-            app.UseAuthentication();
-
-            app.UseLykkeConfiguration(ex =>
+            try
             {
-                string errorMessage;
+                app.UseAuthentication();
 
-                switch (ex)
+                app.UseLykkeConfiguration(options =>
                 {
-                    case InvalidOperationException ioe:
-                        errorMessage = $"Invalid operation: {ioe.Message}";
-                        break;
-                    case ValidationException ve:
-                        errorMessage = $"Validation error: {ve.Message}";
-                        break;
-                    default:
-                        errorMessage = "Technical problem";
-                        break;
-                }
+                    options.SwaggerOptions = _swaggerOptions;
 
-                return ErrorResponse.Create(errorMessage);
-            });
+                    options.DefaultErrorHandler = ex =>
+                    {
+                        string errorMessage;
 
-            ConfigureAutoMapper();
+                        switch (ex)
+                        {
+                            case InvalidOperationException ioe:
+                                errorMessage = $"Invalid operation: {ioe.Message}";
+                                break;
+                            case ValidationException ve:
+                                errorMessage = $"Validation error: {ve.Message}";
+                                break;
+                            default:
+                                errorMessage = "Technical problem";
+                                break;
+                        }
+
+                        return ErrorResponse.Create(errorMessage);
+                    };
+                });
+
+                ConfigureAutoMapper();
+
+                appLifetime.ApplicationStarted.Register(() => StartApplication().GetAwaiter().GetResult());
+                appLifetime.ApplicationStopped.Register(CleanUp);
+            }
+            catch (Exception ex)
+            {
+                _log?.Critical(ex);
+                throw;
+            }
         }
 
         private void ConfigureAutoMapper()
@@ -87,8 +169,40 @@ namespace Lykke.AlgoStore.Service.Logging
             }
             catch (Exception e)
             {
-                _log?.WriteFatalErrorAsync(nameof(Startup), nameof(ConfigureAutoMapper), "", e).Wait();
+                _log.Error(nameof(ConfigureAutoMapper), e, "", nameof(Startup));
 
+                throw;
+            }
+        }
+
+        private async Task StartApplication()
+        {
+            try
+            {
+                _healthNotifier.Notify("Started", Program.EnvInfo);
+#if !DEBUG
+                await Configuration.RegisterInMonitoringServiceAsync(_monitoringServiceUrl, _healthNotifier);
+#endif
+            }
+            catch (Exception ex)
+            {
+                _log.Critical(ex);
+                throw;
+            }
+        }
+
+        private void CleanUp()
+        {
+            try
+            {
+                // NOTE: Job can't receive and process IsAlive requests here, so you can destroy all resources
+                _healthNotifier?.Notify("Terminating", Program.EnvInfo);
+
+                ApplicationContainer.Dispose();
+            }
+            catch (Exception ex)
+            {
+                _log?.Critical(ex);
                 throw;
             }
         }
